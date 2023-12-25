cf2895fd5fe33b17a053476746c5dd83372109fd
Iremkeser37 <145453393+Iremkeser37@users.noreply.github.com>
2023-12-25 12:13:38 +0300

    Add files via upload

---
+++ string örnrkleri/string örnrkleri/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace string_örnrkleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuyut = new System.Windows.Forms.Button();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.btnKucult = new System.Windows.Forms.Button();
+            this.btnSay = new System.Windows.Forms.Button();
+            this.btnMesajVer = new System.Windows.Forms.Button();
+            this.btnBırlestır = new System.Windows.Forms.Button();
+            this.btnSıraGetır = new System.Windows.Forms.Button();
+            this.btnKacis = new System.Windows.Forms.Button();
+            this.btnKarsilastir = new System.Windows.Forms.Button();
+            this.btnBicim = new System.Windows.Forms.Button();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnContanıs = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.lbAd = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnBuyut
+            // 
+            this.btnBuyut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBuyut.Location = new System.Drawing.Point(34, 144);
+            this.btnBuyut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBuyut.Name = "btnBuyut";
+            this.btnBuyut.Size = new System.Drawing.Size(213, 53);
+            this.btnBuyut.TabIndex = 0;
+            this.btnBuyut.Text = "hepsini büyük harfle yaz";
+            this.btnBuyut.UseVisualStyleBackColor = true;
+            this.btnBuyut.Click += new System.EventHandler(this.btnBuyut_Click);
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.Location = new System.Drawing.Point(329, 78);
+            this.lblMesaj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(182, 16);
+            this.lblMesaj.TabIndex = 1;
+            this.lblMesaj.Text = "merhaba dünya ben irem!";
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(34, 78);
+            this.txtMesaj.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMesaj.Multiline = true;
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(211, 29);
+            this.txtMesaj.TabIndex = 2;
+            // 
+            // btnKucult
+            // 
+            this.btnKucult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKucult.Location = new System.Drawing.Point(34, 204);
+            this.btnKucult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKucult.Name = "btnKucult";
+            this.btnKucult.Size = new System.Drawing.Size(213, 50);
+            this.btnKucult.TabIndex = 3;
+            this.btnKucult.Text = "hepsini küçük harfle yaz";
+            this.btnKucult.UseVisualStyleBackColor = true;
+            this.btnKucult.Click += new System.EventHandler(this.btnKucult_Click);
+            // 
+            // btnSay
+            // 
+            this.btnSay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSay.Location = new System.Drawing.Point(34, 262);
+            this.btnSay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSay.Name = "btnSay";
+            this.btnSay.Size = new System.Drawing.Size(213, 48);
+            this.btnSay.TabIndex = 4;
+            this.btnSay.Text = "kac harf var";
+            this.btnSay.UseVisualStyleBackColor = true;
+            this.btnSay.Click += new System.EventHandler(this.btnSay_Click);
+            // 
+            // btnMesajVer
+            // 
+            this.btnMesajVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMesajVer.Location = new System.Drawing.Point(34, 318);
+            this.btnMesajVer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMesajVer.Name = "btnMesajVer";
+            this.btnMesajVer.Size = new System.Drawing.Size(213, 46);
+            this.btnMesajVer.TabIndex = 5;
+            this.btnMesajVer.Text = "mesaj ver";
+            this.btnMesajVer.UseVisualStyleBackColor = true;
+            this.btnMesajVer.Click += new System.EventHandler(this.btnMesajVer_Click);
+            // 
+            // btnBırlestır
+            // 
+            this.btnBırlestır.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBırlestır.Location = new System.Drawing.Point(34, 370);
+            this.btnBırlestır.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBırlestır.Name = "btnBırlestır";
+            this.btnBırlestır.Size = new System.Drawing.Size(213, 52);
+            this.btnBırlestır.TabIndex = 6;
+            this.btnBırlestır.Text = "birlestir";
+            this.btnBırlestır.UseVisualStyleBackColor = true;
+            this.btnBırlestır.Click += new System.EventHandler(this.btnBırlestır_Click);
+            // 
+            // btnSıraGetır
+            // 
+            this.btnSıraGetır.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSıraGetır.Location = new System.Drawing.Point(34, 430);
+            this.btnSıraGetır.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSıraGetır.Name = "btnSıraGetır";
+            this.btnSıraGetır.Size = new System.Drawing.Size(213, 53);
+            this.btnSıraGetır.TabIndex = 7;
+            this.btnSıraGetır.Text = "sıra getir";
+            this.btnSıraGetır.UseVisualStyleBackColor = true;
+            this.btnSıraGetır.Click += new System.EventHandler(this.btnSıraGetır_Click);
+            // 
+            // btnKacis
+            // 
+            this.btnKacis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKacis.Location = new System.Drawing.Point(332, 145);
+            this.btnKacis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKacis.Name = "btnKacis";
+            this.btnKacis.Size = new System.Drawing.Size(213, 52);
+            this.btnKacis.TabIndex = 8;
+            this.btnKacis.Text = "kacıs karakteri";
+            this.btnKacis.UseVisualStyleBackColor = true;
+            this.btnKacis.Click += new System.EventHandler(this.btnKacıs_Click);
+            // 
+            // btnKarsilastir
+            // 
+            this.btnKarsilastir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKarsilastir.Location = new System.Drawing.Point(332, 262);
+            this.btnKarsilastir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKarsilastir.Name = "btnKarsilastir";
+            this.btnKarsilastir.Size = new System.Drawing.Size(213, 48);
+            this.btnKarsilastir.TabIndex = 9;
+            this.btnKarsilastir.Text = "karsılastır";
+            this.btnKarsilastir.UseVisualStyleBackColor = true;
+            this.btnKarsilastir.Click += new System.EventHandler(this.btnKarsilastir_Click);
+            // 
+            // btnBicim
+            // 
+            this.btnBicim.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBicim.Location = new System.Drawing.Point(332, 204);
+            this.btnBicim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBicim.Name = "btnBicim";
+            this.btnBicim.Size = new System.Drawing.Size(213, 50);
+            this.btnBicim.TabIndex = 10;
+            this.btnBicim.Text = "bicim";
+            this.btnBicim.UseVisualStyleBackColor = true;
+            this.btnBicim.Click += new System.EventHandler(this.btnBicim_Click);
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCompare.Location = new System.Drawing.Point(332, 318);
+            this.btnCompare.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(213, 46);
+            this.btnCompare.TabIndex = 11;
+            this.btnCompare.Text = "compare kullanımı";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRemove.Location = new System.Drawing.Point(332, 430);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(213, 53);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnContanıs
+            // 
+            this.btnContanıs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnContanıs.Location = new System.Drawing.Point(332, 370);
+            this.btnContanıs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnContanıs.Name = "btnContanıs";
+            this.btnContanıs.Size = new System.Drawing.Size(213, 52);
+            this.btnContanıs.TabIndex = 13;
+            this.btnContanıs.Text = "contanis kullanımı";
+            this.btnContanıs.UseVisualStyleBackColor = true;
+            this.btnContanıs.Click += new System.EventHandler(this.btnContanıs_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnReplace.Location = new System.Drawing.Point(34, 490);
+            this.btnReplace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(213, 53);
+            this.btnReplace.TabIndex = 14;
+            this.btnReplace.Text = "raplece kullanımı";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(332, 490);
+            this.btnSplit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(213, 53);
+            this.btnSplit.TabIndex = 15;
+            this.btnSplit.Text = "splite";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // lbAd
+            // 
+            this.lbAd.FormattingEnabled = true;
+            this.lbAd.ItemHeight = 16;
+            this.lbAd.Location = new System.Drawing.Point(571, 139);
+            this.lbAd.Name = "lbAd";
+            this.lbAd.Size = new System.Drawing.Size(223, 404);
+            this.lbAd.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(809, 555);
+            this.Controls.Add(this.lbAd);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.btnContanıs);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.btnBicim);
+            this.Controls.Add(this.btnKarsilastir);
+            this.Controls.Add(this.btnKacis);
+            this.Controls.Add(this.btnSıraGetır);
+            this.Controls.Add(this.btnBırlestır);
+            this.Controls.Add(this.btnMesajVer);
+            this.Controls.Add(this.btnSay);
+            this.Controls.Add(this.btnKucult);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.btnBuyut);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuyut;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.Button btnKucult;
+        private System.Windows.Forms.Button btnSay;
+        private System.Windows.Forms.Button btnMesajVer;
+        private System.Windows.Forms.Button btnBırlestır;
+        private System.Windows.Forms.Button btnSıraGetır;
+        private System.Windows.Forms.Button btnKacis;
+        private System.Windows.Forms.Button btnKarsilastir;
+        private System.Windows.Forms.Button btnBicim;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnContanıs;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.ListBox lbAd;
+    }
+}
+
---
+++ string örnrkleri/string örnrkleri/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace string_örnrkleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuyut_Click(object sender, EventArgs e)
+        {
+            string mesaj = lblMesaj.Text;
+            lblMesaj.Text = mesaj.ToUpper();
+        }
+
+        private void btnKucult_Click(object sender, EventArgs e)
+        {
+            string mesaj = lblMesaj.Text;
+            lblMesaj.Text = mesaj.ToLower();
+        }
+
+        private void btnSay_Click(object sender, EventArgs e)
+        {
+           string mesaj = $"toplam {lblMesaj.Text.Length} karakter vardır.";
+           MessageBox.Show(mesaj);
+            
+        }
+
+        private void btnMesajVer_Click(object sender, EventArgs e)
+        {
+            lblMesaj.Text = $"merhaba dünya ben {txtMesaj.Text}";
+        }
+
+        private void btnBırlestır_Click(object sender, EventArgs e)
+        {
+            string mesaj1 = "merhaba dünya";
+            string mesaj2 = "ben irem";
+            string yeniMesaj = string.Concat(mesaj1,mesaj2);
+            lblMesaj.Text = yeniMesaj;
+        }
+
+        private void btnSıraGetır_Click(object sender, EventArgs e)
+        {
+            string mesaj = lblMesaj.Text;
+            // mesaj[4] // char türündedir
+            MessageBox.Show($"6.sıradaki harf:{mesaj[5]}");
+        }
+
+        private void btnKacıs_Click(object sender, EventArgs e)
+        {
+            string mesaj = "Merhaba ben \"irem\". \rNasılsın?";
+            lblMesaj.Text = mesaj;
+        }
+
+        private void btnKarsilastir_Click(object sender, EventArgs e)
+        {
+            string isim1 = "yasemin";
+            string isim2 = "yasemin";
+            string isim3 = "irem";
+
+            bool durum1 = isim1.Equals(isim2);
+            bool durum2 = isim1 == isim2;
+
+            bool durum3 = isim1.Equals(isim3);
+            bool durum4 = isim1 == isim3;
+
+            string mesaj = $"durum1:{durum1} --> durum2 {durum2} \rdurum3 :{durum3} --> durum4 : {durum4}";
+            MessageBox.Show(mesaj);
+
+
+
+
+
+        }
+
+        private void btnBicim_Click(object sender, EventArgs e)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            string mesaj = $"{a} + {b} +{c}";
+            MessageBox.Show(mesaj);
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            string isim1 = "İrem";
+            string isim2 = "Ceren";
+            string isim3 = "Yasemin";
+            string isim4 = "Ela";
+            string isim5 = "kumsal";
+            
+            int sonuc1 = string.Compare(isim1, isim2); //0
+            int sonuc2 = string.Compare(isim1, isim3); //1
+            int sonuc3 = string.Compare(isim1, isim4); //-1
+            int sonuc4 = string.Compare(isim1, isim5); //1
+            int sonuc5 = string.Compare(isim1, isim2,true); //0
+
+
+
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string mesaj1 = lblMesaj.Text.Remove( 7);
+            string mesaj2 = lblMesaj.Text.Remove(0, 4);
+            MessageBox.Show(mesaj1);
+            MessageBox.Show(mesaj2);
+        }
+
+        private void btnContanıs_Click(object sender, EventArgs e)
+        {
+            bool durum = lblMesaj.Text.Contains(txtMesaj.Text);
+            MessageBox.Show($"{txtMesaj.Text}.....{durum}");
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string mesaj = lblMesaj.Text.Replace("irem",txtMesaj.Text);
+            lblMesaj.Text = mesaj;
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+           lbAd.DataSource = lblMesaj.Text.Split(' ').ToList();
+        }
+    }
+}
